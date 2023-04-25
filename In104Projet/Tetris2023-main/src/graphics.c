9a3d385a96cac01de229b5c0c942ac5bff7c6bc7
MagaliCASA <Magali.casamayou@gmail.com>
2023-04-25 16:23:05 +0200

    makefile, read.me and graphics.c changed

--- In104Projet/Tetris2023-main/src/graphics.c
+++ In104Projet/Tetris2023-main/src/graphics.c
@@ -6,21 +6,19 @@ void init_graphics() {
     render_changed = false //the game hasn't started, open a window
     window = SDL_CreateWindow(WINDOW_TITLE, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, flags);
     //window is already defined in init.h (its type: SDL_Window*)
-    if (window==NULL) {
+    if (window==NULL) { //if the previous function did not work, window = NULL, so, an error message pops up.
         fprintf(stderr,
                 "\nTTF_Init Error:  %s\n",
                 SDL_GetError());
         exit(1);
     }
-
 }
 
 void setRenderChanged() {
     render_changed = true;
-} //ok
+} //the function changes the bool render_changed to true, meaning that the render has to be changed
 
 void preRender() {
-
     SDL_SetRenderTarget(render, display);
 }  //ok
 
--- in103/Tetris_complet/src/input.h
+++
@@ -1,6 +0,0 @@
-#include "defs.h"
-#include "tetris.h"
-
-Tetris_Action TETROMINO_ACTION;
-
-void getInput();
